8e325c9eeaf52fb4299e06609a53a537963a432d
IAmACoder0816 <vprodigys@gmail.com>
2018-01-18 14:49:08 -0500

    Started lesson

---
+++ landing_page/src/Functional_JavaScript/high_OrderFunctions.js
@@ -0,0 +1,12 @@
+/* Learning Functional Programming in Javascript by Youtube channel:  Fun Fun Function.  This is Part 1. */
+
+
+var triple = function(x) {
+	return x * 3
+}
+
+var pancakes = triple;
+
+pancakes(9);
+
+
