5b60ba14142ef619a07461278d95c855dbc9947c
IAmACoder0816 <vprodigys@gmail.com>
2018-01-18 15:46:07 -0500

    For Loop Function, What do you think

--- landing_page/src/Functional_JavaScript/high_OrderFunctions.js
+++ landing_page/src/Functional_JavaScript/high_OrderFunctions.js
@@ -1,12 +1,30 @@
 /* Learning Functional Programming in Javascript by Youtube channel:  Fun Fun Function.  This is Part 1. */
 
 
-var triple = function(x) {
-	return x * 3
+//var triple = function(x) {
+//	return x * 3
+//}
+
+//var pancakes = triple;
+
+//pancakes(9);
+
+
+// Lesson on Filter
+
+var seaCreatures = [
+	{name:  'Nemo',		species: 'Clown Fish'},
+	{name:  'Dory',		species: 'Blue Tang'},
+	{name:  'Squirt',	species: 'Sea Turtle'},
+	{name:	'Ray', 		species: 'Sting Ray'},
+	{name: 	'Peanut',	species: 'Jelly Fish'},
+	{name:  'Jaws',		species: 'White Shark'},
+	{name: 	'James',	species: 'White Shark'}
+]
+
+
+var White Shark = []
+for (var i = 0; i < seaCreatures.length; i++) {
+	if(seaCreatures[i].species === 'Great White Shark')
+		Great White Shark.push(seaCreatures[i])
 }
-
-var pancakes = triple;
-
-pancakes(9);
-
-
