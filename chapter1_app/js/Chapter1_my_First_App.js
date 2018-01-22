f25e33da476b6360135afed044eb91c99b848fa3
Yclept <nickmwise@gmail.com>
2018-01-21 21:13:05 -0800

    Completed hello world app.

--- chapter1_app/js/Chapter1_my_First_App.js
+++ chapter1_app/js/Chapter1_my_First_App.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  var tool = new Tool();
+  var c = Shape.Circle(200,200,80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';
 
-  tool.onMouseDown = function(event) {
-    var c = Shape.Circle(event.point.x, event.point.y, 20);
-    c.fillColor = 'green';
-  };
   paper.view.draw();
 
   console.log('Chapter1_my_First_App.js loaded');
@@ -29,3 +31,12 @@ $(document).ready(function(){
 //     circle3.fillColor = 'white';
 //   }
 // }
+
+//Example 2.
+/*var tool = new Tool();
+
+tool.onMouseDown = function(event) {
+  var c = Shape.Circle(event.point.x, event.point.y, 20);
+  c.fillColor = 'green';
+};
+*/
