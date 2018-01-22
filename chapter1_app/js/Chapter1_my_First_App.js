2cec2767d75c019be195cf80adf3fe2142118f03
Yclept <nickmwise@gmail.com>
2018-01-21 20:48:19 -0800

    having fun with paint.

--- chapter1_app/js/Chapter1_my_First_App.js
+++ chapter1_app/js/Chapter1_my_First_App.js
@@ -4,8 +4,20 @@ $(document).ready(function(){
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  var circle = Shape.Circle(200, 200, 50);
-  circle.fillColor = 'green';
+  var circle;
+  var circle2;
+  var circle3;
+
+  for(var i=10; i<400; i+=20) {
+    for(var j=10; j<400; j+=20) {
+      circle = Shape.Circle(i, j, 5);
+      circle2 = Shape.Circle(i, j, 2.5);
+      circle3 = Shape.Circle(i, j, 0.75);
+      circle.fillColor = 'green';
+      circle2.fillColor = 'black';
+      circle3.fillColor = 'white';
+    }
+  }
 
   paper.view.draw();
 
