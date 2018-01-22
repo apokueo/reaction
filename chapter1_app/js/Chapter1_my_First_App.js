210b487c2d91dfdf44d8c59d854af132b23d279c
Yclept <nickmwise@gmail.com>
2018-01-21 21:00:02 -0800

    add event listener to object.

--- chapter1_app/js/Chapter1_my_First_App.js
+++ chapter1_app/js/Chapter1_my_First_App.js
@@ -4,22 +4,28 @@ $(document).ready(function(){
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  var circle;
-  var circle2;
-  var circle3;
+  var tool = new Tool();
 
-  for(var i=10; i<400; i+=20) {
-    for(var j=10; j<400; j+=20) {
-      circle = Shape.Circle(i, j, 5);
-      circle2 = Shape.Circle(i, j, 2.5);
-      circle3 = Shape.Circle(i, j, 0.75);
-      circle.fillColor = 'green';
-      circle2.fillColor = 'black';
-      circle3.fillColor = 'white';
-    }
-  }
-
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+  };
   paper.view.draw();
 
   console.log('Chapter1_my_First_App.js loaded');
 });
+ // EXAMPLE 1.
+// var circle;
+// var circle2;
+// var circle3;
+//
+// for(var i=10; i<400; i+=20) {
+//   for(var j=10; j<400; j+=20) {
+//     circle = Shape.Circle(i, j, 5);
+//     circle2 = Shape.Circle(i, j, 2.5);
+//     circle3 = Shape.Circle(i, j, 0.75);
+//     circle.fillColor = 'green';
+//     circle2.fillColor = 'black';
+//     circle3.fillColor = 'white';
+//   }
+// }
