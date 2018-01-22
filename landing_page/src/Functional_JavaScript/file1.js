5b36440a4becd5387faffec47c50b0ae5468f137
IAmACoder0816 <vprodigys@gmail.com>
2018-01-22 03:03:16 -0500

    Learning functions

---
+++ landing_page/src/Functional_JavaScript/file1.js
@@ -0,0 +1,14 @@
+//Studying functions more closely
+
+function add(num1, num2, num3) {
+	return num1 + num2 + num3
+};
+
+var sum = add
+sum(22, 11, 33);
+
+
+function helloWorld() {
+	for (i = 0; i < 100; i++)
+		console.log("Hello");
+}
