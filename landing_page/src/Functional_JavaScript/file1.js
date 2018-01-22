afa5a3b1229e2a136d7edb6fd9841a4ca7cfaacf
IAmACoder0816 <vprodigys@gmail.com>
2018-01-22 03:24:42 -0500

    Updated file

--- landing_page/src/Functional_JavaScript/file1.js
+++ landing_page/src/Functional_JavaScript/file1.js
@@ -1,14 +1,15 @@
 //Studying functions more closely
 
 function add(num1, num2, num3) {
-	return num1 + num2 + num3
-};
+	return num1 + num2 + num3;
+}
 
 var sum = add
 sum(22, 11, 33);
 
 
 function helloWorld() {
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++){
 		console.log("Hello");
+	}
 }
