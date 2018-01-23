f027393fa3fe984941d831dc95d0505cf3b94455
IAmACoder0816 <vprodigys@gmail.com>
2018-01-23 15:53:15 -0500

    Updated Chapter1 for JS

--- landing_page/src/Six_Weeks_of_JavaScript/chapter1_your_first_application/main.js
+++ landing_page/src/Six_Weeks_of_JavaScript/chapter1_your_first_application/main.js
@@ -6,8 +6,37 @@ $(document).ready(function() {
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
 
-	// TODO
+	var c = Shape.Circle(200, 200, 50);
+	c.fillColor = 'green';
+
+	var c;
+	for (x = 25; x < 100; x += 50) {
+		for(var y = 25; y < 400; y += 50) {
+			c = Shape.Circle(x, y, 20);
+			c.fillColor = 'green';
+		}
+	}
+
+	var tool = new Tool();
+
+	tool.onMouseDown = function(event) {
+		var c = Shape.Circle(event.point.x, event.point.y, 20);
+		c.fillColor = 'green';
+	};
 	
+/* Note we could save ourselves a little typing by passing the point directly to the circle (instead of passing the x and y coordinates 
+	separately): 
+		var c = Shape.Circle(event.point, 20); */
+	
+	var c = Shape.Circle(200, 200, 80);
+	c.fillColor = 'black';
+	var text = new PointText(200, 200);
+	text.justification = 'center';
+	text.fillColor = 'white';
+	text.fontSize = 20;
+	text.content = 'hello world';
+
+
 	paper.view.draw();
 //	console.log('main.js loaded');
 });
