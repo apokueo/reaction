dde37fbee106e758c90df54ed901eaff7a413f96
IAmACoder0816 <vprodigys@gmail.com>
2018-01-23 15:16:12 -0500

    Updated Chapter1 Application

--- landing_page/src/Six_Weeks_of_JavaScript/chapter1_your_first_application/main.js
+++ landing_page/src/Six_Weeks_of_JavaScript/chapter1_your_first_application/main.js
@@ -1,2 +1,18 @@
 // 1st file created is for JavaScript.
-console.log('main.js loaded');
+//console.log('main.js loaded');
+
+$(document).ready(function() {
+	'use strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+	// TODO
+	
+	paper.view.draw();
+//	console.log('main.js loaded');
+});
+
+
+
+
+
