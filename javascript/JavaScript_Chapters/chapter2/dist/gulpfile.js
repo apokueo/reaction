09eb7bb715b382f17dc079930cc3bd54f01d8179
IAmACoder0816 <vprodigys@gmail.com>
2018-01-25 06:13:36 -0500

    Modify gulpfile

--- javascript/JavaScript_Chapters/chapter2/dist/gulpfile.js
+++ javascript/JavaScript_Chapters/chapter2/dist/gulpfile.js
@@ -18,4 +18,23 @@ gulp.src("es6/**/*.js")
 gulp.src("public/es6/**/*.js")
 	.pipe(babel())
 	.pipe(gulp.dest("public/dist"));
+})
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function() {
+	// Run ESLint
+	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
+	// Node source
+	gulp.src("es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
+	// browser source
+	gulp.src("public/es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
 });
